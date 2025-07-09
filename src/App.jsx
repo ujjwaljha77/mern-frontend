9f03584cb7f59f664aa06348a3cab08ac20ef19e
ujjwaljha77 <jhaujjwal1970@gmail.com>
2025-07-09 10:55:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// in app.jsx 
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './assets/components/Home.jsx';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  
+
+  return (
+    <> 
+    <div className="App-Container">
+      <h1 style={{ backgroundColor: "orange" }}>MERN Frontend</h1>
+       <Home age={21} />
+        <h3>This is footer</h3>
+      </div>
+        
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home({ age }) {
+//   let name = "ujjwal";
+
+  if(age>18) return <h1>welcome</h1>;
+  else return <h1>not allowed</h1>
+
+//   return (
+//     <>
+//       <div style={{ backgroundColor: 'green', color: 'white' }}>
+//         Hello {name}, you are {age} years old
+//       </div>
+//       <p>
+//         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, quasi!
+//       </p>
+//     </>
+//   );
+// }
