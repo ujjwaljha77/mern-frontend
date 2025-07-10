e6622c3dd2ceaa1f230c91d95f325c2371725fcd
ujjwaljha77 <jhaujjwal1970@gmail.com>
2025-07-10 11:49:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-// in app.jsx 
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import Home from './assets/components/Home.jsx';
-import Register from './assets/components/Register.jsx';
+import Register from "./assets/components/Register";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
-
   return (
-    <> 
     <div className="App-Container">
       <h1 style={{ backgroundColor: "orange" }}>MERN Frontend</h1>
-       <Home age={10} />
-       <form/>
-        <h3>This is footer</h3>
+      {/* <Home age={10} /> */}
+      
+      <div className="registration-section">
+        <Register />
       </div>
-        
-    </>
-  )
+
+      <h3>This is footer</h3>
+    </div>
+  );
 }
 
-export default App
-
-
-
-
-
+export default App;
--- src/assets/components/Register.jsx
+++ src/assets/components/Register.jsx
@@ -1,12 +1,95 @@
-
+import "./Register.css";
+// import { useRef } from "react";
 import { useState } from "react";
+import axios from "axios";
 export default function Register() {
-
-<form>
-    <input type="text">firts name </input>
-    <input type="text">last name</input>
-    <input type="email">email</input>
-    <input type="number">age</input>
-
-</form>
+  const [user, setUser] = useState({});
+  const [error, setError] = useState();
+  const handleSubmit = async () => {
+    try {
+      const url = "http://localhost:8080/api/users/register";
+      const result = await axios.post(url, user);
+      setError("Data saved successfully");
+    } catch (err) {
+      console.log(err);
+      setError("Something went wrong");
+    }
+  };
+  return (
+    <div>
+      <div>
+        <h2 style={{ backgroundColor: "grey" }}>Registration form</h2>
+        {error}
+        <p>
+          <input
+            type="text"
+            onChange={(e) => setUser({ ...user, firstName: e.target.value })}
+            placeholder="Enter First Name"
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Enter Email Address"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </p>
+        <p>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </p>
+        <p>
+          <button onClick={handleSubmit}>Submit</button>
+        </p>
+      </div>
+    </div>
+  );
 }
+
+// export default function Register() {
+//   const firstName = useRef();
+//   const lastName = useRef();
+//   const email = useRef();
+//   const password = useRef();
+//   const handleSubmit = () => {
+//     const user = {
+//       firstName: firstName.current.value,
+//       lastName: lastName.current.value,
+//       email: email.current.value,
+//       password: password.current.value,
+//     };
+//     console.log(user);
+//   };
+//   return (
+//     <div className="App-Register-Row">
+//       <div style={{ backgroundColor: "white" }}>
+//         <h2>Registration Form</h2>
+//         <p>
+//           <input type="text" placeholder="Enter First Name" ref={firstName} />
+//         </p>
+//         <p>
+//           <input type="text" placeholder="Enter Last Name" ref={lastName} />
+//         </p>
+//         <p>
+//           <input type="text" placeholder="Enter Email Address" ref={email} />
+//         </p>
+//         <p>
+//           <input type="password" placeholder="Enter Password" ref={password} />
+//         </p>
+//         <p>
+//           <button onClick={handleSubmit}>Submit</button>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
